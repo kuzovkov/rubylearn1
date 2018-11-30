da98cc35789461ea28e72bc085dd676614222b04
Aleksandr <kuzovkov.aleksandr@rambler.ru>
2018-11-30 12:50:22 +0300

    ...

---
+++ app/begin.rb
@@ -0,0 +1,119 @@
+#!/usr/bin/env ruby
+
+puts "Hello world!"
+
+puts Math.sqrt(9)
+
+def h(name="World")
+  puts "Hello #{name.capitalize}!"
+end
+
+h("sania")
+h()
+
+
+
+class Greeter
+  def initialize(name = "World")
+    @name = name
+  end
+  def say_hi
+    puts "Hi #{@name}!"
+  end
+  def say_bye
+    puts "Bye #{@name}, come back soon."
+  end
+end
+
+class Greeter
+  attr_accessor :name
+end
+
+
+g = Greeter.new("Sania")
+g.say_hi
+g.say_bye
+
+puts Greeter.instance_methods(false)
+puts g.respond_to?("name")
+puts g.respond_to?("say_hi")
+puts g.respond_to?("to_s")
+puts g.name
+g.name = "Tania"
+g.say_hi
+
+puts 8 % 5
+
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
+
+formatter = "%{first} %{second} %{third} %{fourth}"
+
+puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
+puts formatter % {first: true, second: false, third: true, fourth: false}
+puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+
+puts formatter % {
+    first: "I had this thing.",
+    second: "That you could type up right.",
+    third: "But it didn't sing.",
+    fourth: "So I said goodnight."
+}
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts %q{
+There's something going on here.
+With this weird quote
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+}
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
