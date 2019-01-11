6e3ee35430febe0b4e524a0366ba3680dae33d21
Aleksandr <kuzovkov.aleksandr@rambler.ru>
2019-01-12 00:54:32 +0300

    ...

---
+++ app/base/inheritance.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+class Parent
+
+  def implicit()
+    puts "PARENT implicit()"
+  end
+end
+
+class Child < Parent
+end
+
+dad = Parent.new()
+son = Child.new()
+
+dad.implicit()
+son.implicit()
---
+++ app/base/inheritance2.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+class Parent
+
+  def override()
+    puts "PARENT override()"
+  end
+end
+
+class Child < Parent
+  def override()
+    puts "CHILD override()"
+  end
+end
+
+dad = Parent.new()
+son = Child.new()
+
+dad.override()
+son.override()
